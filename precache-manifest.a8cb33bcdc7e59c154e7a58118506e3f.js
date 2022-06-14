b38939ec31297a13cb563f5ac8b85877c59e7bab
harris2310 <harris2310@hotmail.com>
2022-06-14 17:28:12 +0300

    Updates

---
+++ precache-manifest.a8cb33bcdc7e59c154e7a58118506e3f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9d37fc43f82c8475018ee8798017f5",
+    "url": "/portfolio-final/public/index.html/index.html"
+  },
+  {
+    "revision": "c4b3c059eeeb42f083df",
+    "url": "/portfolio-final/public/index.html/static/css/2.e78a7c17.chunk.css"
+  },
+  {
+    "revision": "b0a3c7c7623f2d5b651c",
+    "url": "/portfolio-final/public/index.html/static/css/main.ed270555.chunk.css"
+  },
+  {
+    "revision": "c4b3c059eeeb42f083df",
+    "url": "/portfolio-final/public/index.html/static/js/2.c5ab5270.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/portfolio-final/public/index.html/static/js/2.c5ab5270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0a3c7c7623f2d5b651c",
+    "url": "/portfolio-final/public/index.html/static/js/main.8d5eb805.chunk.js"
+  },
+  {
+    "revision": "791980a1ef23d3bc0e87",
+    "url": "/portfolio-final/public/index.html/static/js/runtime-main.39847ae0.js"
+  },
+  {
+    "revision": "08b9fb296dcd928e42a90b21cb7d59b3",
+    "url": "/portfolio-final/public/index.html/static/media/Me.08b9fb29.jpg"
+  },
+  {
+    "revision": "9ba7ab8bdfe3f47b72218acc21feb411",
+    "url": "/portfolio-final/public/index.html/static/media/stones.9ba7ab8b.jpg"
+  }
+]);
